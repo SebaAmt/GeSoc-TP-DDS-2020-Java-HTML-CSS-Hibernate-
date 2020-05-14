e037c103159d1a7e85b61c70651a84547fdfe8be
facundosofo <38061708+facundosofo@users.noreply.github.com>
2020-05-14 17:35:59 -0300

    Se agregan test de validación y encriptacion

--- tp-anual/src/test/java/dds/UsuarioTest.java
+++ tp-anual/src/test/java/dds/UsuarioTest.java
@@ -2,6 +2,7 @@ package dds;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.junit.jupiter.api.Test;
 import dds.exception.PasswordException;
 import dds.usuario.Administrador;
 import dds.usuario.Estandar;
+import dds.usuario.Usuario;
 
 public class UsuarioTest {
+	
 
 	@Test
 	@DisplayName("Hay caracteres Repetidos")
@@ -39,5 +42,20 @@ public class UsuarioTest {
 		Exception exception = assertThrows(PasswordException.class, () -> new Administrador("Mauro", "password"));
 		assertEquals("La Password es debil", exception.getMessage());
 	}
+
+	@Test
+	@DisplayName("Utiliza el nombre de usuario")
+	void contraseniaInvalidaPorUtilizarNombreUsuario() {
+		Exception exception = assertThrows(PasswordException.class, () -> new Estandar("jose", "joseramirez95"));
+		assertEquals("No puede incluir el nombre de usuario en la contrasenia", exception.getMessage());
+	}
+
+	@Test
+	@DisplayName("Contrasenia encriptada")
+	public void testIntegrante1() throws PasswordException {
+		Usuario pablo = new Estandar("pabloM","rojocai7L");
+		assertNotEquals("rojocai7L", pablo.password());
+	}
+
 
 }
