e9a00783f9c383e8838dfbdeb00faa93d6b56f7b
Martin Di Paolo <martindp91@gmail.com>
2020-05-13 21:20:07 -0300

    Subo EntidadJuridica y CategoriaEntidadJuridica

---
+++ tp-anual/src/main/java/EntidadJuridica.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class EntidadJuridica {
+
+    private String razonSocial;
+    private String nombreFicticio;
+    private String cuit;
+    private String direccionPostal;
+    private String codigoInscripcionIGJ;
+    private CategoriaEntidadJuridica categoria;
+    List<EntidadBase> entidadesBase = new ArrayList<EntidadBase>();
+    List<Egreso> egresos = new ArrayList<Egreso>();
+
+    public EntidadJuridica(String razonSocial, String nombreFicticio, String cuit, String direccionPostal, String codigoInscripcionIGJ) {
+        this.razonSocial = razonSocial;
+        this.nombreFicticio = nombreFicticio;
+        this.cuit = cuit;
+        this.direccionPostal = direccionPostal;
+        this.codigoInscripcionIGJ = codigoInscripcionIGJ;
+    }
+
+    public void nuevoEgreso(Egreso egreso){
+        this.egresos.add(egreso);
+    }
+
+    public void agregarEntidadBase(EntidadBase entidadBase){
+        this.entidadesBase.add(entidadBase);
+    }
+
+}
