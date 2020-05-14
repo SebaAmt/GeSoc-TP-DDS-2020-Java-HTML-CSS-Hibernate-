993c3d854eb2ebca240961f77eb0590bff69b904
Esteban <erocca@est.frba.utn.edu.ar>
2020-05-14 16:33:22 -0300

    Agrego EntidadBase

--- tp-anual/src/main/java/EntidadBase.java
+++ tp-anual/src/main/java/EntidadBase.java
@@ -1,2 +1,24 @@
 public class EntidadBase {
+	private String nombreFicticio;
+	private String descripcion;
+	private EntidadJuridica entidadJuridica;
+	private ArrayList<Egreso> egresos = new ArrayList<>();
+	
+	public EntidadBase(String nombreFicticio, String descripcion, EntidadJuridica entidadJUridica){
+		this.nombreFicticio = nombreFicticio;
+		this.descripcion = descripcion;
+		this.entidadJuridica = entidadJuridica;
+	}
+	
+	public void entidadJuridica(){
+		return this.entidadJuridica;
+	}
+	
+	public void nuevoEgreso(Egreso egreso){
+        this.egresos.add(egreso);
+    }
+
+    public ArrayList<Egreso> egresos(){
+    	return egresos;
+    }
 }
