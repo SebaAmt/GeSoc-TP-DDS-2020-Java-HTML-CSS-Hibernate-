f90834de02aa71b2dd929e786905a7a82d707519
Martin Di Paolo <martindp91@gmail.com>
2020-05-19 19:47:42 -0300

    Issue #2: saco referencias en entidad base a entidad juridica

--- tp-anual/src/main/java/dds/entidades/EntidadBase.java
+++ tp-anual/src/main/java/dds/entidades/EntidadBase.java
@@ -7,7 +7,6 @@ import java.util.*;
 public class EntidadBase {
 	private String nombreFicticio;
 	private String descripcion;
-	private EntidadJuridica entidadJuridica;
 	private ArrayList<Egreso> egresos = new ArrayList<>();
 
 	public EntidadBase(String nombreFicticio, String descripcion){
@@ -15,16 +14,6 @@ public class EntidadBase {
 		this.descripcion = descripcion;
 	}
 
-	public EntidadBase(String nombreFicticio, String descripcion, EntidadJuridica entidadJUridica){
-		this.nombreFicticio = nombreFicticio;
-		this.descripcion = descripcion;
-		this.entidadJuridica = entidadJuridica;
-	}
-	
-	public EntidadJuridica entidadJuridica(){
-		return this.entidadJuridica;
-	}
-	
 	public void nuevoEgreso(Egreso egreso){
         this.egresos.add(egreso);
     }
