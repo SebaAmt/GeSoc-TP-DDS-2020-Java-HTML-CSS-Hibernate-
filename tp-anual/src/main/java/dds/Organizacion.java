ec19d663b1d8453c8b560fd221593ffff5f67cf1
Martin Di Paolo <martindp91@gmail.com>
2020-05-19 19:49:53 -0300

    Issue #3: saco usuario de la organizacion

--- tp-anual/src/main/java/dds/Organizacion.java
+++ tp-anual/src/main/java/dds/Organizacion.java
@@ -2,7 +2,6 @@ package dds;
 
 import dds.entidades.EntidadBase;
 import dds.entidades.EntidadJuridica;
-import dds.usuario.Usuario;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +10,6 @@ public class Organizacion {
     private String nombre;
     private List<EntidadBase> entidadesBase = new ArrayList<EntidadBase>();
     private List<EntidadJuridica> entidadesJuridicas = new ArrayList<EntidadJuridica>();
-    private Usuario usuario;
 
     public Organizacion(String nombre) {
         this.nombre = nombre;
@@ -23,10 +21,6 @@ public class Organizacion {
 
     public void agregarEntidadJuridica(EntidadJuridica nuevaEntidadJuridica){
         this.entidadesJuridicas.add(nuevaEntidadJuridica);
-    }
-
-    public void asignarUsuario(Usuario usuario){
-        this.usuario = usuario;
     }
     
 }
