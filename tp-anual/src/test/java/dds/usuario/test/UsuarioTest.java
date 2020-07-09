73715b4268808b54fac6320892462014881b3f15
Martin Di Paolo <martindp91@gmail.com>
2020-07-09 20:52:03 -0300

    borro comentario de más

--- tp-anual/src/test/java/dds/usuario/test/UsuarioTest.java
+++ tp-anual/src/test/java/dds/usuario/test/UsuarioTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-//import dds.egreso.CreadorEgreso;
 import dds.exception.PasswordException;
 import dds.initialize.Initialize;
 import dds.usuario.CreadorDeUsuario;
