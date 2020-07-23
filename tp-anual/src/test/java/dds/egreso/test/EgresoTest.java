ec2d9492924619665838bc0cace64dc3cb28d29b
SebaAmt <seba.amt@hotmail.com>
2020-07-22 21:56:53 -0300

    Agrego tests para nuevos métodos de la clase Egreso relacionados a sus etiquetas y a la nueva funcionalidad necesaria de las entidades para generar reportes.

--- tp-anual/src/test/java/dds/egreso/test/EgresoTest.java
+++ tp-anual/src/test/java/dds/egreso/test/EgresoTest.java
@@ -1,5 +1,6 @@
 package dds.egreso.test;
 
+
 import dds.documentoComercial.DocumentoComercial;
 import dds.documentoComercial.TipoDocumentoComercial;
 import dds.egreso.CreadorMoneda;
@@ -13,11 +14,18 @@ import dds.mediosDePago.TipoMedioDePago;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class EgresoTest {
 
@@ -34,8 +42,7 @@ public class EgresoTest {
 		final List<Item> items1 = new ArrayList<>();
 		final CreadorMoneda creadorPesos;
 		final Moneda peso;
-		
-		
+
 		item1 = new Item("Rollo tela", new BigDecimal(300), 3);
 		item2 = new Item("Lamina de cuero", new BigDecimal(300), 4);
 		items1.add(item1);
@@ -45,7 +52,8 @@ public class EgresoTest {
 		peso = creadorPesos.getMoneda();
 		documento2 = new DocumentoComercial(TipoDocumentoComercial.ORDEN_DE_COMPRA, 9999999);
 		medioDePago1 = new MedioDePago(TipoMedioDePago.CAJERO, "159753456");
-		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null, false, null);
+		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null,
+				false, null);
 
 		// Act
 
@@ -72,7 +80,7 @@ public class EgresoTest {
 		final List<Item> items1 = new ArrayList<>();
 		final CreadorMoneda creadorPesos;
 		final Moneda peso;
-	
+
 		item1 = new Item("Rollo tela", new BigDecimal(300), 3);
 		item2 = new Item("Lamina de cuero", new BigDecimal(300), 4);
 		item4 = new Item("Rollo aluminio", new BigDecimal(500), 1);
@@ -83,17 +91,205 @@ public class EgresoTest {
 		peso = creadorPesos.getMoneda();
 		documento2 = new DocumentoComercial(TipoDocumentoComercial.ORDEN_DE_COMPRA, 9999999);
 		medioDePago1 = new MedioDePago(TipoMedioDePago.CAJERO, "159753456");
-		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null, false,
-				null);
+		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null,
+				false, null);
 
 		// Act
 
 		egreso1.agregarItem(item4);
 		int cantidadDeItemsEsperada = items1.size();
-	    int cantidadDeItemsActual = egreso1.getItems().size();
+		int cantidadDeItemsActual = egreso1.getItems().size();
 
 		// Assert
 
 		assertEquals(cantidadDeItemsEsperada, cantidadDeItemsActual);
+	}
+
+	@Test
+	public void comprobarEgresoEtiquetadoCorrectamente() {
+
+		// Arrange
+
+		final Egreso egreso1;
+		final Item item1;
+		final Item item2;
+		final Proveedor proveedor1;
+		final DocumentoComercial documento2;
+		final MedioDePago medioDePago1;
+		final List<Item> items1 = new ArrayList<>();
+		final CreadorMoneda creadorPesos;
+		final Moneda peso;
+
+		item1 = new Item("Rollo tela", new BigDecimal(300), 3);
+		item2 = new Item("Lamina de cuero", new BigDecimal(300), 4);
+		items1.add(item1);
+		items1.add(item2);
+		proveedor1 = new Proveedor("Telas SA", 30258741, null);
+		creadorPesos = new CreadorMoneda(CurrencyID.ARS);
+		peso = creadorPesos.getMoneda();
+		documento2 = new DocumentoComercial(TipoDocumentoComercial.ORDEN_DE_COMPRA, 9999999);
+		medioDePago1 = new MedioDePago(TipoMedioDePago.CAJERO, "159753456");
+		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null,
+				false, null);
+
+		// Act
+		
+		egreso1.etiquetar("amoblamiento");
+		List<String> etiquetasEsperadas = new ArrayList<>();
+		etiquetasEsperadas.add("AMOBLAMIENTO");
+		List<String> etiquetasActuales = egreso1.getEtiquetasAsignadas();
+
+		// Assert
+
+		assertIterableEquals(etiquetasEsperadas, etiquetasActuales);
+	}
+
+	@Test
+	public void comprobarEgresoLanzaExcepcionAlSerEtiquetadoDosVecesIgual() {
+
+		// Arrange
+
+		final Egreso egreso1;
+		final Item item1;
+		final Item item2;
+		final Proveedor proveedor1;
+		final DocumentoComercial documento2;
+		final MedioDePago medioDePago1;
+		final List<Item> items1 = new ArrayList<>();
+		final CreadorMoneda creadorPesos;
+		final Moneda peso;
+
+		item1 = new Item("Rollo tela", new BigDecimal(300), 3);
+		item2 = new Item("Lamina de cuero", new BigDecimal(300), 4);
+		items1.add(item1);
+		items1.add(item2);
+		proveedor1 = new Proveedor("Telas SA", 30258741, null);
+		creadorPesos = new CreadorMoneda(CurrencyID.ARS);
+		peso = creadorPesos.getMoneda();
+		documento2 = new DocumentoComercial(TipoDocumentoComercial.ORDEN_DE_COMPRA, 9999999);
+		medioDePago1 = new MedioDePago(TipoMedioDePago.CAJERO, "159753456");
+		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null,
+				false, null);
+
+		// Act
+
+		egreso1.etiquetar("amoblamiento");
+
+		// Assert
+
+		assertThrows(RuntimeException.class, () -> egreso1.etiquetar("Amoblamiento"),
+				"El egreso ya está etiquetado como: AMOBLAMIENTO");
+	}
+
+	@Test
+	public void comprobarEgresoDesetiquetadoCorrectamente() {
+
+		// Arrange
+
+		final Egreso egreso1;
+		final Item item1;
+		final Item item2;
+		final Proveedor proveedor1;
+		final DocumentoComercial documento2;
+		final MedioDePago medioDePago1;
+		final List<Item> items1 = new ArrayList<>();
+		final CreadorMoneda creadorPesos;
+		final Moneda peso;
+
+		item1 = new Item("Rollo tela", new BigDecimal(300), 3);
+		item2 = new Item("Lamina de cuero", new BigDecimal(300), 4);
+		items1.add(item1);
+		items1.add(item2);
+		proveedor1 = new Proveedor("Telas SA", 30258741, null);
+		creadorPesos = new CreadorMoneda(CurrencyID.ARS);
+		peso = creadorPesos.getMoneda();
+		documento2 = new DocumentoComercial(TipoDocumentoComercial.ORDEN_DE_COMPRA, 9999999);
+		medioDePago1 = new MedioDePago(TipoMedioDePago.CAJERO, "159753456");
+		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null,
+				false, null);
+
+		// Act
+		egreso1.etiquetar("amoblamiento");
+		egreso1.etiquetar("textil");
+		egreso1.desetiquetar("amoblamiento");
+		List<String> etiquetasActuales = egreso1.getEtiquetasAsignadas();
+		boolean resultadoObtenido = etiquetasActuales.contains("AMOBLAMIENTO");
+		// Assert
+
+		assertFalse(resultadoObtenido);
+	}
+
+	@Test
+	public void comprobarEgresoCorrespondeAlMesActual() {
+
+		// Arrange
+
+		final Egreso egreso1;
+		final Item item1;
+		final Item item2;
+		final Proveedor proveedor1;
+		final DocumentoComercial documento2;
+		final MedioDePago medioDePago1;
+		final List<Item> items1 = new ArrayList<>();
+		final CreadorMoneda creadorPesos;
+		final Moneda peso;
+
+		item1 = new Item("Rollo tela", new BigDecimal(300), 3);
+		item2 = new Item("Lamina de cuero", new BigDecimal(300), 4);
+		items1.add(item1);
+		items1.add(item2);
+		proveedor1 = new Proveedor("Telas SA", 30258741, null);
+		creadorPesos = new CreadorMoneda(CurrencyID.ARS);
+		peso = creadorPesos.getMoneda();
+		documento2 = new DocumentoComercial(TipoDocumentoComercial.ORDEN_DE_COMPRA, 9999999);
+		medioDePago1 = new MedioDePago(TipoMedioDePago.CAJERO, "159753456");
+		egreso1 = new Egreso(LocalDate.of(2020, 5, 15).withMonth(LocalDate.now().getMonthValue()), proveedor1,
+				documento2, medioDePago1, peso, items1, null, null, false, null);
+
+		// Act
+
+		boolean valorActual = egreso1.perteneceAlMesActual();
+
+		// Assert
+
+		assertTrue(valorActual);
+	}
+
+	@Test
+	public void comprobarEgresoEstaEtiquetadoComo() {
+
+		// Arrange
+
+		final Egreso egreso1;
+		final Item item1;
+		final Item item2;
+		final Proveedor proveedor1;
+		final DocumentoComercial documento2;
+		final MedioDePago medioDePago1;
+		final List<Item> items1 = new ArrayList<>();
+		final CreadorMoneda creadorPesos;
+		final Moneda peso;
+
+		item1 = new Item("Rollo tela", new BigDecimal(300), 3);
+		item2 = new Item("Lamina de cuero", new BigDecimal(300), 4);
+		items1.add(item1);
+		items1.add(item2);
+		proveedor1 = new Proveedor("Telas SA", 30258741, null);
+		creadorPesos = new CreadorMoneda(CurrencyID.ARS);
+		peso = creadorPesos.getMoneda();
+		documento2 = new DocumentoComercial(TipoDocumentoComercial.ORDEN_DE_COMPRA, 9999999);
+		medioDePago1 = new MedioDePago(TipoMedioDePago.CAJERO, "159753456");
+		egreso1 = new Egreso(LocalDate.of(2020, 5, 15), proveedor1, documento2, medioDePago1, peso, items1, null, null,
+				false, null);
+
+		// Act
+		egreso1.etiquetar("amoblamiento");
+		egreso1.etiquetar("ProVeeDor1");
+
+		boolean resultadoEsperado = egreso1.estaEtiquetadoComo("proveedoR1");
+
+		// Assert
+
+		assertTrue(resultadoEsperado);
 	}
 }
