cb8dcc173eddd858e616546bf94cb05a210bfb49
SebaAmt <seba.amt@hotmail.com>
2020-09-21 22:37:43 -0300

    Agrego clases persistentes al persistence.xml y corrijo import en ContextTest

--- tp-anual/src/test/java/db/ContextTest.java
+++ tp-anual/src/test/java/db/ContextTest.java
@@ -2,9 +2,10 @@ package db;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+
 
 public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
 
