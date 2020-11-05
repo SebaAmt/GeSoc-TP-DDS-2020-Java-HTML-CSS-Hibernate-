c42a477ea7069b7b6bba0d575200d2240e104d37
Martin Di Paolo <martindp91@gmail.com>
2020-11-04 22:30:38 -0300

    Inicializo algunos objetos y los persisto para ya tenerlos en la base y facilitar las pruebas/navegación por el sitio

--- tp-anual/src/test/java/db/ContextTest.java
+++ tp-anual/src/test/java/db/ContextTest.java
@@ -2,6 +2,13 @@ package db;
 
 import static org.junit.Assert.*;
 
+import model.entidades.EntidadBase;
+import model.entidades.EntidadJuridica;
+import model.entidades.Organizacion;
+import model.usuario.CreadorDeUsuario;
+import model.usuario.TipoUsuario;
+import model.usuario.Usuario;
+import model.validacionesContrasenias.*;
 import org.junit.jupiter.api.Test;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
@@ -16,7 +23,26 @@ public class ContextTest extends AbstractPersistenceTest implements WithGlobalEn
 
 	@Test
 	public void contextUpWithTransaction() throws Exception {
-		withTransaction(() -> {});
+		withTransaction(() -> {
+
+			// Inicializo algunos objetos para ya tenerlos en la base y facilitar las pruebas/navegación por el sitio
+			ValidadorDeContrasenias validador = new ValidadorDeContrasenias();
+			CreadorDeUsuario creadorSimple = new CreadorDeUsuario(validador);
+
+			Usuario usuarioPrueba = creadorSimple.crearUsuario("user1", "prueba", TipoUsuario.ESTANDAR);
+			Organizacion valve = new Organizacion("Valve");
+			EntidadJuridica blackMesa = new EntidadJuridica("Black Mesa SRL", "Black Mesa", "123456789", "Direccion 123", "123");
+			EntidadBase aperture = new EntidadBase("Aperture Laboratories", "Hacemos portales");
+			valve.agregarEntidadBase(aperture);
+			valve.agregarEntidadJuridica(blackMesa);
+
+			usuarioPrueba.asignarOrganizacion(valve);
+
+			persist(blackMesa);
+			persist(aperture);
+			persist(valve);
+			entityManager().persist(usuarioPrueba);
+		});
 	}
 
 }
