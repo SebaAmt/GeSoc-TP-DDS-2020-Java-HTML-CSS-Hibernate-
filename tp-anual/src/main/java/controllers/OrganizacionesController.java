3fc1c242f42f5dbbf004619124cca0ca044c2386
RomeroLlanosJulian <romero.llanos.julian@gmail.com>
2020-11-13 09:50:35 -0300

    Se agrega filtro de entidades por categoria

--- tp-anual/src/main/java/controllers/OrganizacionesController.java
+++ tp-anual/src/main/java/controllers/OrganizacionesController.java
@@ -1,18 +1,23 @@
 package controllers;
 
 
+import model.categoria.Categoria;
 import model.entidades.EntidadBase;
 import model.entidades.EntidadJuridica;
 import model.entidades.Organizacion;
 import model.usuario.Usuario;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+
+import repositorios.RepositorioCategorias;
 import repositorios.RepositorioOrganizaciones;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class OrganizacionesController implements WithGlobalEntityManager {
 
@@ -30,15 +35,18 @@ public class OrganizacionesController implements WithGlobalEntityManager {
         
         try{
             Organizacion organizacion = RepositorioOrganizaciones.instancia.obtenerOrganizacionPorId(Long.parseLong(id));
+            List<EntidadBase> entidadesBase = organizacion.getEntidadesBase();
+            List<EntidadJuridica> entidadesJuridicas = organizacion.getEntidadesJuridicas();
             if(organizacion == null){
                 response.redirect("/error", 404); // not found
             }
-
             SessionHelper.validarOrganizacionUsuarioLogueado(request, response, Long.parseLong(id));
             Usuario usuarioLogueado = SessionHelper.getUsuarioLogueado(request);
 
             Map<String, Object> modelo = new HashMap<>();
             modelo.put("organizacion", organizacion);
+            modelo.put("entidadesBase", entidadesBase);
+            modelo.put("entidadesJuridicas", entidadesJuridicas);
             
             return new ModelAndView(modelo, "detalle-organizacion.html.hbs");
         }
@@ -47,4 +55,29 @@ public class OrganizacionesController implements WithGlobalEntityManager {
             return null;
         }
     }
+    
+    public ModelAndView getOrganizacionConEntidadesFiltradas(Request request, Response response) {
+    	Long organizacionId = Long.parseLong(request.params(":id"));
+    	Long categoriaId = Long.parseLong(request.queryParams("filtro"));
+    	
+    	Organizacion organizacion = RepositorioOrganizaciones.instancia.obtenerOrganizacionPorId(organizacionId);
+    	Categoria categoria = RepositorioCategorias.instancia.getCategoriaPorId(categoriaId);
+    	
+    	List<EntidadBase> entidadesBase =
+    			categoriaId != null ?
+    					organizacion.getEntidadesBase().stream().filter(e->e.getCategoria().equals(categoria)).collect(Collectors.toList()) :
+    					organizacion.getEntidadesBase();
+    	List<EntidadJuridica> entidadesJuridicas =
+    			categoriaId != null ?
+    					organizacion.getEntidadesJuridicas().stream().filter(e->e.getCategoria().equals(categoria)).collect(Collectors.toList()) :
+    					organizacion.getEntidadesJuridicas();
+    	
+        Map<String, Object> modelo = new HashMap<>();
+        modelo.put("organizacion", organizacion);    	
+        modelo.put("entidadesBase", entidadesBase);
+        modelo.put("entidadesJuridicas", entidadesJuridicas);
+    	
+        return new ModelAndView(modelo, "detalle-organizacion.html.hbs");
+    }
+    
 }
